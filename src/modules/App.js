826c8322fd8140a4046a8beb5ae6312d54271571
Camilo Daza <daza.camilos@gmail.com>
2020-04-17 20:46:35 -0400

    Add: Catalog component

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import SearchBar from './SearchBar/SearchBar';
+import Catalog from './Catalog/Catalog';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <SearchBar></SearchBar>
+      <Catalog></Catalog>
     </div>
   );
 }
---
+++ src/modules/Catalog/Catalog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Catalog.css';
+
+class Catalog extends React.Component {
+    render() {
+        return(
+            <div className='catalog-container'>catalogo</div>
+        )
+    }
+}
+export default Catalog;
