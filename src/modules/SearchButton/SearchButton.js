842b5eb23d6f18e9ef0cf4eb4c22d8fd8fbfcda8
Camilo Daza <daza.camilos@gmail.com>
2020-04-17 20:47:05 -0400

    Add: SearchButton component

--- src/modules/SearchButton/SearchButton.js
+++ src/modules/SearchButton/SearchButton.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 
 import './SearchButton.css';
 
 class SearchButton extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return(
             <input 
                 type = 'button' 
                 value = {this.props.searchButtonText}
-                onClick = {this.handleClick}
+                onClick = {()=>this.props.onClick()}
             />
         );
     }
