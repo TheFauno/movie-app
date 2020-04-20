5f5ad962bc144f952294f735e229ce373fb24832
Camilo Daza <daza.camilos@gmail.com>
2020-04-20 00:24:30 -0400

    ADD: Storate utility functions

---
+++ src/libs/FavouriteMovieStorage.js
@@ -0,0 +1,30 @@
+export function IsFavourite(imdbID) {
+  try {
+    const favouriteslocalStorageArray = localStorage.getItem('favouriteMovies');
+    const isFavourite = favouriteslocalStorageArray.includes(imdbID);
+    console.log('Es favorito?'+isFavourite);
+    return isFavourite;
+  }
+  catch (e) {
+    return null;
+  }
+}
+
+function RemoveFromStorage(imdbID) {
+  let existing = localStorage.getItem('favouriteMovies');
+  existing = existing ? existing.split(',') : [];
+  existing = existing.filter(storedImdbID => storedImdbID !== imdbID);
+  localStorage.setItem('favouriteMovies', existing.toString());;  
+}
+
+function AddToStorage(imdbID) {
+  let existing = localStorage.getItem('favouriteMovies');
+  existing = existing ? existing.split(',') : [];
+  existing.push(imdbID);
+  localStorage.setItem('favouriteMovies', existing.toString());
+}
+
+export function ToogleFavourite(imdbID) {
+  let isFavourite = IsFavourite(imdbID);
+  isFavourite ? RemoveFromStorage(imdbID) : AddToStorage(imdbID)
+}
